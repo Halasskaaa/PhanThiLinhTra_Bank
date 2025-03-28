f4b9666119b4bb1fe08ec9d33bfe904dde55334c
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-03-28 10:42:37 +0100

    Owner class creation done(?)

---
+++ bank/Owner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+	internal abstract class Owner
+	{
+		private string name;
+
+		protected Owner(string name)
+		{
+			this.name = name;
+		}
+
+		public string Name { get => name; set => name = value; }
+	
+	}
+}
