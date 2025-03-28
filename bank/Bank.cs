e0170cf41b8a105bf9785e41d4881849424c500d
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-03-28 11:38:25 +0100

    Bank class done

--- bank/Bank.cs
+++ bank/Bank.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,58 @@ namespace Bank
 {
 	internal class Bank
 	{
+		BankAccount[] accounts;
+
+		public Bank(int maxAccountCount)
+		{
+			accounts = new BankAccount[maxAccountCount];
+		}
+		public BankAccount NewAccount(Owner owner, double creditLimit)
+		{
+			if (creditLimit == 0)
+			{
+				return new SavingsAccount(owner);
+			}
+			if (creditLimit < 0)
+			{
+				return new CreditAccount(owner, creditLimit);
+			}
+		}
+		 
+		public double TotalBalance(Owner owner)
+		{
+			double osszeg = 0;
+
+            foreach (var account in accounts)
+            {
+				osszeg += account.Balance;
+            }
+			return osszeg;
+        }
+
+		public BankAccount MaximalBalanceAccount(Owner owner)
+		{
+			BankAccount maxBankAccount = accounts[0];
+
+            for (int i = 0; i < accounts.Length; i++)
+            {
+				if (accounts[i].Balance > maxBankAccount.Balance) maxBankAccount = accounts[i];
+				return maxBankAccount;
+			}
+        }
+
+		public double TotalCredit(Owner owner)
+		{
+			double osszeg = 0;
+
+            foreach (var account in accounts)
+            {
+                if (account is CreditAccount)
+				{
+					osszeg += (account as CreditAccount).CreditLimit;
+				}
+            }
+			return osszeg;
+        }
 	}
 }
